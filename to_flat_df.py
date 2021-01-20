59e20cb1c094b58287c6860f87b667ef21701125
Roy <royassisg@gmail.com>
2021-01-20 16:06:55 +0200

    Restructure

--- to_flat_df.py
+++ to_flat_df.py
@@ -1,26 +1,11 @@
 import json
 import pandas as pd
-from utils import get_dict_list, get_flat_dict
-
-
-def rec(mylist, base_dict, added_dict):
-    dictlist = get_dict_list(base_dict)
-    plain_dict = get_flat_dict(base_dict)
-
-    if not dictlist:
-        mylist.append(plain_dict | added_dict)
-        return
-
-    for newdict in dictlist:
-        rec(mylist, newdict, plain_dict | added_dict)
-
-    return mylist
-
+from utils import rec
 
 with open("sample.json", "r") as fp:
     data = json.load(fp)
 
-result = rec(list(), data, dict())
+result = rec(data)
 
 df = pd.DataFrame(result)
 
--- utils.py
+++ utils.py
@@ -6,3 +6,21 @@ def get_dict_list(d):
     for key, val in d.items():
         if type(val) == list:
             return val
+
+
+def rec_base(mylist, base_dict, added_dict):
+    dictlist = get_dict_list(base_dict)
+    plain_dict = get_flat_dict(base_dict)
+
+    if not dictlist:
+        mylist.append(plain_dict | added_dict)
+        return
+
+    for newdict in dictlist:
+        rec_base(mylist, newdict, plain_dict | added_dict)
+
+    return mylist
+
+
+def rec(data):
+    return rec_base(list(), data, dict())
