0c8c5b7706152cdd6b7eefb38cc703d75964227a
Roy <royassisg@gmail.com>
2021-01-20 08:24:04 +0200

    Fix filenaming mistake

--- to_flat_dfs.py
+++ to_flat_dfs.py
@@ -3,19 +3,18 @@ import pandas as pd
 from utils import get_dict_list, get_flat_dict
 
 
-# TODO: implement
-# def rec(mylist, base_dict, added_dict):
-#     dictlist = get_dict_list(base_dict)
-#     plain_dict = get_flat_dict(base_dict)
-#
-#     if not dictlist:
-#         mylist.append(plain_dict | added_dict)
-#         return
-#
-#     for newdict in dictlist:
-#         rec(mylist, newdict, plain_dict | added_dict)
-#
-#     return mylist
+def rec(mylist, base_dict, added_dict):
+    dictlist = get_dict_list(base_dict)
+    plain_dict = get_flat_dict(base_dict)
+
+    if not dictlist:
+        mylist.append(plain_dict | added_dict)
+        return
+
+    for newdict in dictlist:
+        rec(mylist, newdict, plain_dict | added_dict)
+
+    return mylist
 
 
 with open("sample.json", "r") as fp:
